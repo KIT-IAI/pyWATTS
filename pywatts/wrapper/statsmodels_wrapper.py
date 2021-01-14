d2073f28f9a6741effb8302e4bb8f528ee390903
marianturowski <marian.turowski@kit.edu>
2021-01-14 11:18:27 +0100

    first revision based on merge request

--- pywatts/wrapper/statsmodels_wrapper.py
+++ pywatts/wrapper/statsmodels_wrapper.py
@@ -1,40 +1,91 @@
 import pickle
+from typing import Dict
 
 import numpy as np
 import statsmodels as sm
 import xarray as xr
 
-from pywatts.core.exceptions.kind_of_transform_does_not_exist_exception import KindOfTransformDoesNotExistException, \
-    KindOfTransform
 from pywatts.core.filemanager import FileManager
 from pywatts.wrapper.base_wrapper import BaseWrapper
 
 
 class StatsmodelsWrapper(BaseWrapper):
     """
-        A wrapper class for statsmodels modules. 
-        :param module: The statsmodels module to wrap
-        :type module: statsmodels.base.BaseEstimator
-        :param name: The name of the module
-        :type name: str
-        """
+    Wrapper for statsmodels modules.
 
-    # Alternative: sm.base.Model?
-    def __init__(self, module: sm.tsa.base.tsa_model.TimeSeriesModel, name: str = None):
+    :param module: The statsmodels module to wrap
+    :type module: sm.tsa.base.tsa_model.TimeSeriesModel
+    :param name: The name of the module
+    :type name: str
+    :param module_kwargs: The module keyword arguments necessary for creating the statsmodel module
+    :type module_kwargs: dict
+    :param fit_kwargs: The optional fit keyword arguments for fitting the model
+    :type fit_kwargs: dict
+    :param predict_kwargs: The optional predict keyword arguments for predicting with the model (except start and end)
+    :type predict_kwargs: dict
+    :param model_params: The fitted model parameters
+    :type xr.Dataset
+    """
+
+    def __init__(self, module: sm.tsa.base.tsa_model.TimeSeriesModel, name: str = None, module_kwargs=None,
+                 fit_kwargs=None, predict_kwargs=None, model_params=None):
         if name is None:
             name = module.__class__.__name__
         super().__init__(name)
+        if module_kwargs is None:
+            module_kwargs = {}
+        if fit_kwargs is None:
+            fit_kwargs = {}
+        if predict_kwargs is None:
+            predict_kwargs = {}
         self.module = module
+        self.module_kwargs = module_kwargs
+        self.fit_kwargs = fit_kwargs
+        self.predict_kwargs = predict_kwargs
+        self.model_params = model_params
+
+    def get_params(self) -> Dict[str, object]:
+        """
+        Returns the parameters of the statsmodels module.
+
+        :return: A dict containing the module keyword arguments, the fit keyword arguments, the predict keyword
+        arguments and the fitted model parameters
+        :rtype: dict
+        """
+        return {
+            "module_kwargs": self.module_kwargs,
+            "fit_kwargs": self.fit_kwargs,
+            "predict_kwargs": self.predict_kwargs,
+            "model_params": self.model_params
+        }
+
+    def set_params(self, module_kwargs=None, fit_kwargs=None, predict_kwargs=None, model_params=None):
+        """
+        Set the parameters of the statsmodels wrapper
+
+        :param module_kwargs: keyword arguments for the statsmodel module.
+        :param fit_kwargs: keyword arguments for the fit method.
+        :param predict_kwargs: keyword arguments for the predict method.
+        :param model_params: parameters of the fitted model
+        """
+        if module_kwargs:
+            self.module_kwargs = module_kwargs
+        if fit_kwargs:
+            self.fit_kwargs = fit_kwargs
+        if predict_kwargs:
+            self.predict_kwargs = predict_kwargs
+        if model_params:
+            self.model_params = model_params
 
     def fit(self, x: xr.Dataset, y: xr.Dataset = None):
         """
-        Fit the sklearn module
+        Fits the statsmodels module
+
         :param x: input data
-        :param y: target data
+        :param y: target data (not needed)
         """
         x = self._dataset_to_statsmodels_input(x)
-        y = self._dataset_to_statsmodels_input(y)
-        self.module.fit(x, y)
+        self.model_params = self.module(x, **self.module_kwargs).fit(**self.fit_kwargs).params
         self.is_fitted = True
 
     @staticmethod
@@ -45,14 +96,13 @@ class StatsmodelsWrapper(BaseWrapper):
         for data_var in x.data_vars:
             data_array = x[data_var]
             if result is not None:
-                result = np.concatenate([result, data_array.values.reshape((len(data_array.values), -1))], axis=1)
+                result = np.concatenate([result, data_array.values.reshape(-1)], axis=1)
             else:
-                result = data_array.values.reshape((len(data_array.values), -1))
+                result = data_array.values.reshape(-1)
         return result
 
     @staticmethod
     def _statsmodels_output_to_dataset(x: xr.Dataset, prediction, name: str) -> xr.Dataset:
-
         coords = (
             # first dimension is number of batches. We assume that this is the time.
             ("time", list(x.coords.values())[0].to_dataframe().index.array),
@@ -63,28 +113,27 @@ class StatsmodelsWrapper(BaseWrapper):
         return xr.Dataset(data)
 
     def transform(self, x: xr.Dataset) -> xr.Dataset:
-        def transform(self, x:xr.Dataset) -> xr.Dataset:
-               x_np = self._dataset_to_statsmodels_input(x)
-               prediction = self.module.transform(x_np)
-               return self._statsmodels_output_to_dataset(x, prediction, self.name)
-        Transforms a dataset or predicts the result with the wrapped statsmodels module
+        """
+        Predicts the result with the wrapped statsmodels module
+
         :param x: the input dataset
         :return: the transformed output
         """
-        x_np = self._dataset_to_statsmodels_input(x)
-
-        if "transform" in dir(self.module):
-            prediction = self.module.transform(x_np)
-        elif "predict" in dir(self.module):
-            prediction = self.module.predict(x_np)
-        else:
-            raise KindOfTransformDoesNotExistException(
-                f"The statsmodels module in {self.name} does not have a predict or transform method",
-                KindOfTransform.PREDICT_TRANSFORM)
-
+        time_data = x.to_dataframe().iloc[:, 0]
+        start = time_data.index.min()[1]
+        end = time_data.index.max()[1]
+        prediction = self.module.predict(self.model_params, start, end, **self.predict_kwargs)
         return self._statsmodels_output_to_dataset(x, prediction, self.name)
 
     def save(self, fm: FileManager):
+        """
+        Saves the statsmodels wrapper and the containing model
+
+        :param fm: FileManager for getting the path
+        :type fm: FileManager
+        :return: Dictionary with additional information
+        :rtype: Dict
+        """
         json = super().save(fm)
         file_path = fm.get_path(f'{self.name}.pickle')
         with open(file_path, 'wb') as outfile:
@@ -95,6 +144,8 @@ class StatsmodelsWrapper(BaseWrapper):
     @classmethod
     def load(cls, load_information) -> 'StatsmodelsWrapper':
         """
+        Loads a statsmodels wrapper
+
         :param load_information: Information for reloading the StatsmodelsWrapper
         :type load_information: Dict
         :return: The reloaded StatsmodelsWrapper
--- tests/unit/wrapper/test_statsmodels_wrapper.py
+++ tests/unit/wrapper/test_statsmodels_wrapper.py
@@ -1,27 +1,150 @@
+import os
 import unittest
+from typing import Optional
+from unittest.mock import MagicMock, call
 
 import xarray as xr
-from statsmodels.tsa.ar_model import AutoReg
+import pandas as pd
+import numpy as np
 
 from pywatts.wrapper.statsmodels_wrapper import StatsmodelsWrapper
+
+stored_module = {
+    "class": "StatsmodelsWrapper",
+    "model": os.path.join("pipe1", "AR.h5"),
+    "module": "pywatts.wrapper.statsmodels_wrapper",
+    "name": "AR",
+    'is_fitted': False,
+    "params": {
+        "module_kwargs": {
+            "lags": [1, 4]
+        },
+        "fit_kwargs": {
+            "cov_type": "nonrobust"
+        },
+        "predict_kwargs": {
+            "dynamic": True
+        },
+        "model_params": {
+            "scale": 2.0
+        }
+    }
+}
 
 
 class TestStatsmodelsWrapper(unittest.TestCase):
 
-    def test_fit_Regression(self):
-        data = xr.Dataset({"test": xr.DataArray([1, 2, 3, 4, 5])})
-        ar_model = AutoReg(data, lags=[1, 2])
-        wrapper = StatsmodelsWrapper(module=ar_model)
-        test_ar_results = wrapper.fit(data)
-        expected_ar_results = ar_model.fit()
-        self.assertEqual(test_ar_results, expected_ar_results)
+    def setUp(self) -> None:
+        self.statsmodels_mock: Optional[MagicMock] = MagicMock()
+        self.statsmodels_wrapper = StatsmodelsWrapper(self.statsmodels_mock, module_kwargs={"lags": [1, 2]},
+                                                      fit_kwargs={}, predict_kwargs={}, model_params={})
 
-    def test_predict_Regression(self):
-        data = xr.Dataset({"test": xr.DataArray([1, 2, 3, 4, 5])})
-        ar_model = AutoReg(data, lags=[1, 2])
-        ar_results = ar_model.fit()
-        ar_results_param = ar_results.params
-        wrapper = StatsmodelsWrapper(module=ar_model)
-        test_prediction = wrapper.transform(ar_results_param)
-        expected_prediction = ar_model.predict(ar_results_param)
-        self.assertEqual(test_prediction, expected_prediction)
+    def tearDown(self) -> None:
+        self.statsmodels_wrapper: Optional[StatsmodelsWrapper] = None
+        self.statsmodels_mock = None
+
+    def test_get_params(self):
+        self.assertEqual(self.statsmodels_wrapper.get_params(),
+                         {'module_kwargs': {'lags': [1, 2]},
+                          'fit_kwargs': {},
+                          'predict_kwargs': {},
+                          'model_params': {}
+                          })
+
+    def test_set_params(self):
+        self.assertEqual(self.statsmodels_wrapper.get_params(),
+                         {'module_kwargs': {'lags': [1, 2]},
+                          'fit_kwargs': {},
+                          'predict_kwargs': {},
+                          'model_params': {}
+                          })
+        self.statsmodels_wrapper.set_params(module_kwargs={"lags": [1, 4]},
+                                            fit_kwargs={"cov_type": "nonrobust"},
+                                            predict_kwargs={"dynamic": True},
+                                            model_params={"scale": 2.0})
+        self.assertEqual(self.statsmodels_wrapper.get_params(),
+                         {"module_kwargs": {"lags": [1, 4]},
+                          "fit_kwargs": {"cov_type": "nonrobust"},
+                          "predict_kwargs": {"dynamic": True},
+                          "model_params": {"scale": 2.0}
+                          })
+
+    def test_fit(self):
+        self.statsmodels_wrapper.set_params(module_kwargs={"lags": [1, 2]}, fit_kwargs={}, predict_kwargs={},
+                                            model_params={})
+        time = pd.date_range('2000-01-01', freq='24H', periods=7)
+        data = xr.Dataset(
+            {'TestData': (['time', 'measurement'], [[2, 1], [3, 2], [4, 4], [5, 8], [6, 16], [7, 32], [8, 64]]),
+             'time': time})
+        self.statsmodels_wrapper.fit(data)
+
+        # assert fit is called
+        self.statsmodels_mock.module.assert_called_once_with(module_kwargs={"lags": [1, 2]})
+        self.statsmodels_mock.fit.assert_called_once()
+
+        # assert correct arguments when calling fit
+        args = self.statsmodels_mock.fit.call_args
+        np.testing.assert_equal(args[1]["x"]["TestData"],
+                                np.array([[2, 1], [3, 2], [4, 4], [5, 8], [6, 16], [7, 32], [8, 64]]))
+        self.assertEqual(len(args[1]["x"]), 1)
+
+        # assert is_fitted is set to true
+        self.assertTrue(self.statsmodels_wrapper.is_fitted)
+
+    def test_predict(self):
+        self.statsmodels_wrapper.set_params(module_kwargs={"lags": [1, 2]}, fit_kwargs={}, predict_kwargs={},
+                                            model_params={})
+        time = pd.date_range('2000-01-01', freq='24H', periods=7)
+        data = xr.Dataset(
+            {'TestData': (['time', 'measurement'], [[2, 1], [3, 2], [4, 4], [5, 8], [6, 16], [7, 32], [8, 64]]),
+             'time': time})
+        self.statsmodels_wrapper.transform(data)
+
+        # assert transform is called
+        self.statsmodels_mock.transform.assert_called_once()
+
+        # assert correct arguments when calling transform
+        args = self.statsmodels_mock.predict.call_args
+        np.testing.assert_equal(args[1], '2000-01-01 00:00:00')
+        np.testing.assert_equal(args[2], '2000-01-07 00:00:00')
+        np.testing.assert_equal(args[3], {})
+
+    def test_save(self):
+        fm_mock = MagicMock()
+        fm_mock.get_path.return_value = os.path.join("new_path", "to_somewhere", "StatsmodelsWrapper.pickle")
+        json = self.statsmodels_wrapper.save(fm_mock)
+        self.statsmodels_mock.save.assert_called_once_with(
+            filepath=os.path.join("new_path", "to_somewhere", "StatsmodelsWrapper.pickle"))
+        fm_mock.get_path.has_calls(call(os.path.join("to_somewhere", "StatsmodelsWrapper.pickle")),
+                                   any_order=True)
+        self.assertEqual(json, {'class': 'StatsmodelsWrapper',
+                                'is_fitted': False,
+                                'model': os.path.join("new_path", "to_somewhere", "StatsmodelsWrapper.pickle"),
+                                'module': 'pywatts.wrapper.statsmodels_wrapper',
+                                'name': 'StatsmodelsWrapper',
+                                })
+
+    def test_load(self, load_module_mock):
+        new_statsmodels_mock = MagicMock()
+        load_module_mock.return_value = new_statsmodels_mock
+        new_statsmodels_wrapper = StatsmodelsWrapper.load(stored_module)
+        calls_open = [call(filepath=os.path.join("pipe1", "AR.pickle"))]
+
+        load_module_mock.assert_has_calls(calls_open, any_order=True)
+        self.assertEqual(load_module_mock.call_count, 1)
+        self.assertEqual(new_statsmodels_mock, new_statsmodels_wrapper.module)
+        self.assertEqual(new_statsmodels_wrapper.get_params(),
+                         {
+                             "module_kwargs": {
+                                 "lags": [1, 4]
+                             },
+                             "fit_kwargs": {
+                                 "cov_type": "nonrobust"
+                             },
+                             "predict_kwargs": {
+                                 "dynamic": True
+                             },
+                             "model_params": {
+                                 "scale": 2.0
+                             }
+                         })

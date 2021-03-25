8e0239dfa4e33e6b655e4ceb7f928362382fca05
benHeid <benHeid@users.noreply.github.com>
2021-03-25 17:23:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pywatts",
-    version="0.0.1.dev",
+    version="0.1.0",
     packages=setuptools.find_packages(),
 
     install_requires=['scikit-learn', 'cloudpickle', 'holidays', 'xarray', 'numpy', 'pandas', 'matplotlib',

df55c933c3ac22887213950eabdda70c9f322b17
Oliver Neumann <oliver.neumann@kit.edu>
2021-05-31 08:56:32 +0200

    Added fit method to FunctionModule (#95)
    * Added fit method to FunctionModule

--- tests/unit/wrapper/test_function_module.py
+++ tests/unit/wrapper/test_function_module.py
@@ -2,34 +2,50 @@ import os
 import unittest
 from unittest.mock import MagicMock, patch
 
-from pywatts.wrapper.function_module import FunctionModule
+from pywatts.wrapper import FunctionModule
 
 
 class TestFunctionModule(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.mocked_function = MagicMock()
-        self.function_module = FunctionModule(self.mocked_function)
+        # define mock functions for fit and transform
+        self.fit_function = MagicMock()
+        self.transform_function = MagicMock()
+
+        # define two function modules one using only a transform method
+        # and another one using fit and transform methods
+        self.transform_module = FunctionModule(self.transform_function)
+        self.fit_transform_module = FunctionModule(self.transform_function, self.fit_function)
 
     def tearDown(self) -> None:
-        self.function_module = None
+        self.transform_module = None
 
     def test_transform(self):
         data_mock = MagicMock()
-        self.function_module.transform(input_data=data_mock)
+        self.transform_module.fit(input_data=data_mock)
+        self.transform_module.transform(input_data=data_mock)
 
-        self.mocked_function.assert_called_once_with(input_data=data_mock)
+        self.transform_function.assert_called_once_with(input_data=data_mock)
+
+    def test_fit_transform(self):
+        data_mock = MagicMock()
+        self.fit_transform_module.fit(input_data=data_mock)
+        self.fit_transform_module.transform(input_data=data_mock)
+
+        self.fit_function.assert_called_once_with(input_data=data_mock)
+        self.transform_function.assert_called_once_with(input_data=data_mock)
 
     @patch('pywatts.wrapper.function_module.cloudpickle')
     @patch("builtins.open")
     def test_save(self, open_mock, cloudpickle_mock):
         fm_mock = MagicMock()
         fm_mock.get_path.return_value = os.path.join("testpath", "FunctionModule.pickle")
-        json = self.function_module.save(fm_mock)
+        json = self.transform_module.save(fm_mock)
         open_mock.assert_called_once_with(os.path.join("testpath", "FunctionModule.pickle"), 'wb')
         cloudpickle_mock.dump.assert_called_once()
         self.assertEqual(json, {
             "params": {},
+            "is_fitted": True,
             "name": "FunctionModule",
             "class": "FunctionModule",
             "module": "pywatts.wrapper.function_module",
@@ -39,7 +55,7 @@ class TestFunctionModule(unittest.TestCase):
     @patch('pywatts.wrapper.function_module.cloudpickle')
     @patch("builtins.open")
     def test_load(self, open_mock, cloudpickle_mock):
-        self.function_module.load({
+        self.transform_module.load({
             "pickled_module": "pickle_path.pickle"
         })
         open_mock.assert_called_once_with("pickle_path.pickle", "rb")

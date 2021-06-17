f8c0e8af116db8fe452647d89ddc023268cf678d
benHeid <benHeid@users.noreply.github.com>
2021-06-17 15:35:49 +0200

    Fix linting errors. Mention Summaries in README.md

--- pywatts/core/base_step.py
+++ pywatts/core/base_step.py
@@ -43,7 +43,8 @@ class BaseStep(ABC):
         self.buffer: Dict[str, xr.DataArray] = {}
         self.training_time = None
 
-    def get_result(self, start: pd.Timestamp, end: Optional[pd.Timestamp], buffer_element: str = None,                  return_all=False):
+    def get_result(self, start: pd.Timestamp, end: Optional[pd.Timestamp], buffer_element: str = None,
+                   return_all=False):
         """
         This method is responsible for providing the result of this step.
         Therefore,

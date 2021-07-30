90ecb462aabd25bf68408f7a6ebea4ab616d5036
Kristof Kraus <kristofkraus@gmail.com>
2021-07-30 16:55:45 +0200

    Implemented MAPE as MetricBase and adapted tests.

---
+++ pywatts/summaries/mape_summary.py
@@ -0,0 +1,25 @@
+import logging
+
+import numpy as np
+
+from pywatts.summaries.metric_base import MetricBase
+
+logger = logging.getLogger(__name__)
+
+
+class MAPE(MetricBase):
+    """
+    Module to calculate the Mean Absolute Percentage Error (MAPE)
+
+    :param offset: Offset, which determines the number of ignored values in the beginning for calculating the MAPE.
+                   Default 0
+    :type offset: int
+    :param filter_method: Filter which should performed on the data before calculating the MAPE.
+    :type filter_method: Callable[[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]
+    """
+
+
+    def _apply_metric(self, p, t):
+        non_zero_indices = t.nonzero()[0][t.nonzero()[0] >= self.offset]
+        return np.mean(np.abs((p[non_zero_indices] - t[non_zero_indices]) / t[non_zero_indices]))
+

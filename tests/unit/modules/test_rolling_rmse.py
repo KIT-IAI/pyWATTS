b118dc82083fb68c71f121fa9ad85084e392203f
benHeid <benHeid@users.noreply.github.com>
2021-07-30 10:37:48 +0200

    Fix tests after xarray upgrade

--- tests/unit/modules/test_rolling_rmse.py
+++ tests/unit/modules/test_rolling_rmse.py
@@ -33,13 +33,9 @@ class TestRollingRMSE(unittest.TestCase):
 
         test_data = xr.Dataset({"testCol": ("time", xr.DataArray([-2, -1, 0, 1, 2]).data),
                                 "predictCol1": ("time", xr.DataArray([2, -3, 3, 1, -2]).data),
-                                "predictCol2": ("time", xr.DataArray([4, 4, 3, -2, 1]).data), "time": time, "timeTest": time})
+                                "predictCol2": ("time", xr.DataArray([4, 4, 3, -2, 1]).data), "time": time})
 
-        y = xr.DataArray(np.array([-2, -1, 0, 1, 2]),
-                                       coords={"timeTest": time},
-                                       dims=["timeTest"])
-
-        test_result = self.rolling_rmse.transform(y=y, gt=test_data['testCol'],
+        test_result = self.rolling_rmse.transform(y=test_data['testCol'] gt=test_data['testCol'],
                                                   pred1=test_data['predictCol1'],
                                                   pred2=test_data['predictCol2'])
         expected_result = xr.DataArray(np.array([[0.0, 4, 6],

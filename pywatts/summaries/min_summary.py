dc02d1a8d8e50bfdeb811578d624615bdd2ee6a4
Kristof Kraus <kristofkraus@gmail.com>
2021-08-02 14:10:15 +0200

    Feature/105 min max calculator (#138)
    * max error measure.
    
    * Added min error measure with tests 
    
    * Updated docs/api/pywatts.modules.rst with min_,max_error.

---
+++ pywatts/summaries/min_summary.py
@@ -0,0 +1,24 @@
+import logging
+
+import numpy as np
+
+from pywatts.summaries.metric_base import MetricBase
+
+logger = logging.getLogger(__name__)
+
+
+class MinErr(MetricBase):
+    """
+    Module to calculate the minimal absolute error
+
+    :param offset: Offset, which determines the number of ignored values in the beginning for calculating the min.
+                   Default 0
+    :type offset: int
+    :param filter_method: Filter which should performed on the data before calculating the min.
+    :type filter_method: Callable[[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]
+    """
+
+
+    def _apply_metric(self, p, t):
+        return np.min(np.abs((p[self.offset:] - t[self.offset:])))
+

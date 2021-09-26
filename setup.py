7eca2c40959962ad29dcbf1e33da450d51c07c33
benHeid <benHeid@users.noreply.github.com>
2021-09-26 13:17:25 +0200

    add licence and classifier to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.1.0",
     packages=setuptools.find_packages(),
 
-    install_requires=['scikit-learn < 1.0', 'cloudpickle', 'holidays', 'xarray>=0.19', 'numpy', 'pandas', 'matplotlib',
+    install_requires=['scikit-learn >= 1.0', 'cloudpickle', 'holidays', 'xarray>=0.19', 'numpy', 'pandas', 'matplotlib',
                       'tensorflow>=2', 'workalendar',  'statsmodels', 'tabulate'],
     extras_require={
         'dev': [
@@ -17,6 +17,18 @@ setuptools.setup(
     },
     author="pyWATTS-TEAM",
     author_email="pywatts-team@iai.kit.edu",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License"
+        "Topic :: Software Development :: Libraries",
+        "Natural Language :: English"
+        "Operating System :: OS Independent"
+
+    ],
     description="A python time series pipelining project",
     keywords="preprocessing time-series machine-learning",
 )

6269557d1da95a0af179be5b4d3b2c95ebfca3f5
Benedikt Heidrich <benHeid@users.noreply.github.com>
2021-09-30 11:36:45 +0200

    fix versions for python 3.9 (#155)
    * fix versions for python 3.9
    
    * add licence and classifier to setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,33 @@ import setuptools
 
 setuptools.setup(
     name="pywatts",
-    version="0.1.0",
+    version="0.2.0",
     packages=setuptools.find_packages(),
 
-    install_requires=['scikit-learn', 'cloudpickle', 'holidays', 'xarray', 'numpy', 'pandas', 'matplotlib',
-                      'tensorflow', 'workalendar',  'statsmodels', 'tabulate'],
+    install_requires=['scikit-learn >= 1.0', 'cloudpickle', 'holidays', 'xarray>=0.19', 'numpy', 'pandas', 'matplotlib',
+                      'tensorflow>=2', 'workalendar',  'statsmodels', 'tabulate'],
     extras_require={
         'dev': [
             "pytest",
-            "sphinx",
+            "sphinx>=4",
             "pylint",
             "pytest-cov"
         ]
     },
     author="pyWATTS-TEAM",
     author_email="pywatts-team@iai.kit.edu",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License"
+        "Topic :: Software Development :: Libraries",
+        "Natural Language :: English"
+        "Operating System :: OS Independent"
+
+    ],
     description="A python time series pipelining project",
     keywords="preprocessing time-series machine-learning",
 )

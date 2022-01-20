70184894ca7c9bfb2fb5210ce922e6fe114ded40
SMEISEN <meisenbacher.stefan@web.de>
2022-01-20 10:45:23 +0100

    add load and save

--- pywatts/modules/postprocessing/condition.py
+++ pywatts/modules/postprocessing/condition.py
@@ -1,9 +1,10 @@
-from typing import Dict, Callable
-
+import cloudpickle
 import xarray as xr
 import numpy as np
+from typing import Dict, Callable
 
 from pywatts.core.base import BaseTransformer
+from pywatts.core.filemanager import FileManager
 from pywatts.utils._xarray_time_series_utils import numpy_to_xarray
 from pywatts.core.exceptions import InvalidInputException
 
@@ -79,3 +80,40 @@ class Condition(BaseTransformer):
         result = np.where(self.condition(dependency_data), if_true_data, if_false_data)
 
         return numpy_to_xarray(result, dependency, self.name)
+
+    def save(self, fm: FileManager):
+        """
+        Saves the Conditional module to JSON file
+
+        :param fm: A FileManager, from which the path where the JSON file is saved is fetches
+        :type fm: FileManager
+        :return: Dictionary with name, parameters, related module and class, and path to the file
+        :rtype: Dict
+        """
+        json_module = super().save(fm)
+        file_path = fm.get_path(f'{self.name}.pickle')
+        with open(file_path, 'wb') as outfile:
+            cloudpickle.dump(self, file=outfile)
+        json_module["pickled_module"] = file_path
+        json_module.pop("params")
+        return json_module
+
+    @classmethod
+    def load(cls, load_information):
+        """
+        Loads a condition from a JSON file and creates the corresponding Conditional
+
+        :param load_information: JSON file of the Conditional
+        :type load_information: Dict
+        :return: conditional module from file
+        :rtype: Conditional
+
+        .. warning::
+            This method use pickle for loading the module. Note that this is not safe.
+            Consequently, load only modules you trust.
+            For more details about pickling see https://docs.python.org/3/library/pickle.html
+
+        """
+        with open(load_information["pickled_module"], 'rb') as pickle_file:
+            module = cloudpickle.load(pickle_file)
+        return module

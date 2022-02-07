9ff03adbbbb47cfebe25ce8a764df7f56eb4238f
SMEISEN <meisenbacher.stefan@web.de>
2022-02-07 09:29:33 +0100

    resolve requested changes

--- pywatts/modules/preprocessing/average.py
+++ pywatts/modules/preprocessing/average.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 import xarray as xr
 import numpy as np
+from typing import List
 
 from pywatts.core.base import BaseTransformer
 from pywatts.utils._xarray_time_series_utils import numpy_to_xarray
@@ -13,7 +14,7 @@ class Average(BaseTransformer):
     By default simple averaging is applied.
     """
 
-    def __init__(self, weights: list = None, name: str = "Average"):
+    def __init__(self, weights: List[float] = None, name: str = "Average"):
         """ Initialize the averaging step.
         :param weights: List of individual weights of each given time series for weighted averaging.
         :type weights: list, optional
@@ -31,7 +32,7 @@ class Average(BaseTransformer):
             "weights": self.weights
         }
 
-    def set_params(self, weights: list = None):
+    def set_params(self, weights: List[float] = None):
         """ Set or change Average object parameters.
         :param weights: List of individual weights of each given time series for weighted averaging.
         :type weights: list, optional
@@ -40,20 +41,20 @@ class Average(BaseTransformer):
             self.weights = weights
 
     def transform(self, **kwargs) -> xr.DataArray:
-        """ Aggregate the given time series by simple or weighted averaging.
-        :return: Xarray dataset aggregated by simple or weighted averaging.
+        """ Aggregate the given time data_array by simple or weighted averaging.
+        :return: xarray DataArray aggregated by simple or weighted averaging.
         :rtype: xr.DataArray
         """
 
-        list_of_series = []
+        list_of_data_arrays = []
         list_of_indexes = []
-        for series in kwargs.values():
-            list_of_indexes.append(series.indexes)
-            list_of_series.append(series.data)
+        for data_array in kwargs.values():
+            list_of_indexes.append(data_array.indexes)
+            list_of_data_arrays.append(data_array.data)
 
         if not all(all(index) == all(list_of_indexes[0]) for index in list_of_indexes):
-            raise ValueError("The indexes of the given time series for averaging do not match")
+            raise ValueError("The indexes of the given time data_array for averaging do not match")
 
-        result = np.average(list_of_series, axis=0, weights=self.weights)
+        result = np.average(list_of_data_arrays, axis=0, weights=self.weights)
 
-        return numpy_to_xarray(result, series, self.name)
+        return numpy_to_xarray(result, data_array, self.name)

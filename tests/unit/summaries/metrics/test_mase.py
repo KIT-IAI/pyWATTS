63823fdb6471550bb41a157e7e1c20c8460e6ade
Benedikt Heidrich <benHeid@users.noreply.github.com>
2022-02-07 13:05:10 +0100

    Rename module to enable pytest 7.0.0 (#178)

--- tests/unit/summaries/metrics/test_mase.py
+++ tests/unit/summaries/metrics/test_mase.py
@@ -8,7 +8,7 @@ import xarray as xr
 
 from pywatts.core.exceptions.input_not_available import InputNotAvailable
 from pywatts.summaries.mase_summary import MASE
-from tests.unit.summaries.metrics.base_test_metric_base import BaseTestMetricBase
+from tests.unit.summaries.metrics.test_base_metric_base import BaseTestMetricBase
 
 
 class TestMase(BaseTestMetricBase, unittest.TestCase):

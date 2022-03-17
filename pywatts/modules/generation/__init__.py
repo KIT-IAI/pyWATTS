4bdc5eb88d6ea5b03330085a9f622021ab393f43
Oliver Neumann <oliver.neumann@kit.edu>
2022-03-17 14:38:01 +0100

    Adding AnomalyGernation modules (#196)
    * Implemented AnomalyGernation, EnergyAnomalyGeneration, and PowerAnomalyGeneration module

---
+++ pywatts/modules/generation/__init__.py
@@ -0,0 +1,3 @@
+from pywatts.modules.generation.anomaly_generation_module import AnomalyGeneration
+from pywatts.modules.generation.energy_anomaly_generation_module import EnergyAnomalyGeneration
+from pywatts.modules.generation.power_anomaly_generation_module import PowerAnomalyGeneration
